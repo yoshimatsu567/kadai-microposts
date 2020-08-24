1da249a4948855cff32606e08d7d795362434870
kio <lilkiossgang@gmail.com>
2020-08-24 15:24:23 +0000

    post

--- app/User.php
+++ app/User.php
@@ -10,30 +10,31 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    /**
+    * このユーザが所有する投稿。(Micropostsモデルとの関係を定義)
+    */
+    public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
+    
+    /**
+    * このユーザに関係するモデルの件数をロードする。
+    */
+    public function loadRelationshipCounts()
+    {
+        $this->loadCount('microposts');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MicropostsController@index');
 
 //  ユーザー登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
@@ -25,6 +23,6 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function() {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]
-    );
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
